c6f8330f4d521bc17cc53a7918f0a1195323f7ce
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-05 18:11:11 -0400

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
 @echo off
-if exist .git ( git pull || echo Update failed. Attempting to update again... && git stash && git pull ) else ( echo Git not found. Update skiped && pause & exit )
+if exist .git ( git pull || echo Update failed. Attempting to update again... && git stash && git pull ) else ( echo Git not found. Update skiped. && pause & exit )
 echo Update complete. && pause & exit
