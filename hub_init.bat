a732e45aa91dd852f816b08f31bbf3a6672676b8
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-05 19:25:53 -0400

    Fixed broken hub launcher

--- hub_init.bat
+++ hub_init.bat
@@ -1,14 +1,5 @@
 @echo off && cls
-::     Anistick Animium Launcher    ::
-::            Made by Kia           ::
-::       https://anistick.com       ::
-set LAUNCHER_RUN=nofolder
-if not exist installed goto inst
-if exist installed goto inst
-:inst
-npm install
-echo Installed >> installed
-goto noinst
-:noinst
-start npm start
-pause
+::     Anistick Animium Launcher                        ::
+::            Made by Kia And Fixed By Joseph.          ::
+::       https://anistick.com                           ::
+if not exist node_modules ( npm install && npm start ) else ( npm start )
