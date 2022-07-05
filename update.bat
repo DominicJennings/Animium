c82212a389f3340d391826740ee936fa3e3b38eb
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-05 18:29:26 -0400

    Update update.bat

--- update.bat
+++ update.bat
@@ -2,5 +2,5 @@
 ::    Anistick Animium Updater    ::
 ::     Made by Kia and Joseph     ::
 ::      https://anistick.com      ::
-if exist .git ( git pull || echo Update failed. Attempting to update again... && git stash && git pull ) else ( echo Git not found. Update skiped. && pause & exit )
+if exist .git ( git pull || echo Update failed. Attempting to update again... && git stash && git pull || echo i have nothing else to do now. && call cmd.exe ) else ( echo Git not found. Update skiped. && pause & exit )
 echo Update sucessfull. && pause & exit
