f32a00b1a7bde1e2b0c4bea4cfc4f1f87bdc540f
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-05 18:28:30 -0400

    Update update.bat

--- update.bat
+++ update.bat
@@ -3,3 +3,4 @@
 ::     Made by Kia and Joseph     ::
 ::      https://anistick.com      ::
 if exist .git ( git pull || echo Update failed. Attempting to update again... && git stash && git pull ) else ( echo Git not found. Update skiped. && pause & exit )
+echo Update sucessfull. && pause & exit
