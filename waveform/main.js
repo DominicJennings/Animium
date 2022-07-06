b8abba7f93c57b17184e8134ed3ada2ef76518a7
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-06 17:14:10 -0400

    Update main.js

--- waveform/main.js
+++ waveform/main.js
@@ -3,16 +3,15 @@ const folder = process.env.WAVEFORMS_FOLDER;
 
 module.exports = {
 	/**
-	 * Looks for a match in the _WAVEFORMS folder.
-	 * If there's no match found, it returns null.
-	 * @param {string} wfId 
-	 * @returns {Buffer | null}
-	 */
-  load(wfId) { // look for match in folder
-    const match = fs.readdirSync(folder).find(file => file.includes(`${wfId}.wf`));
-    return match ? fs.readFileSync(path.join(folder, match)) : null;
-  },
-
+	* Looks for a match in the _WAVEFORMS folder.
+	* If there's no match found, it returns null.
+	* @param {string} wfId 
+	* @returns {Buffer | null}
+	*/
+	load(wfId) { // look for match in folder
+		const match = fs.readdirSync(folder).find(file => file.includes(`${wfId}.wf`));
+		return match ? fs.readFileSync(path.join(folder, match)) : null;
+	},
 	/**
 	 * Saves the waveform to the _WAVEFORMS folder.
 	 * @param {Buffer} wf 
@@ -20,7 +19,7 @@ module.exports = {
 	 * @returns {boolean}
 	 */
 	save(wf, wfId) {
-    fs.writeFileSync(path.join(folder, `${wfId}.wf`), wf);
+		fs.writeFileSync(path.join(folder, `${wfId}.wf`), wf);
 		return true;
-  }
+	}
 };
