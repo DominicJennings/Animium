112b73dd64de630a1a9a46587b33d64e6c8b2069
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-06 17:12:39 -0400

    Create main.js

---
+++ waveform/main.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const folder = process.env.WAVEFORMS_FOLDER;
+
+module.exports = {
+	/**
+	 * Looks for a match in the _WAVEFORMS folder.
+	 * If there's no match found, it returns null.
+	 * @param {string} wfId 
+	 * @returns {Buffer | null}
+	 */
+  load(wfId) { // look for match in folder
+    const match = fs.readdirSync(folder).find(file => file.includes(`${wfId}.wf`));
+    return match ? fs.readFileSync(path.join(folder, match)) : null;
+  },
+
+	/**
+	 * Saves the waveform to the _WAVEFORMS folder.
+	 * @param {Buffer} wf 
+	 * @param {string} wfId 
+	 * @returns {boolean}
+	 */
+	save(wf, wfId) {
+    fs.writeFileSync(path.join(folder, `${wfId}.wf`), wf);
+		return true;
+  }
+};
