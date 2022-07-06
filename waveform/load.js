f6d0142d96b1d3423f19dba6ef1a9e4990199bb3
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-06 17:17:18 -0400

    Create load.js

---
+++ waveform/load.js
@@ -0,0 +1,19 @@
+const wf = require("./main");
+const loadPost = require("../misc/post_body");
+
+/**
+ * @param {import("http").IncomingMessage} req
+ * @param {import("http").ServerResponse} res
+ * @param {import("url").UrlWithParsedQuery} url
+ * @returns {boolean}
+ */
+module.exports = async function (req, res, url) {
+	if (req.method != "POST" || url.pathname != "/goapi/getWaveform/") return;
+  loadPost(req, res).then(data => {
+    const { wfid: wfId } = data;
+
+    const waveform = wf.load(wfId);
+    waveform ? (res.statusCode = 200, res.end(waveform)) : (res.statusCode = 404, res.end());
+    return true;
+  });
+}
