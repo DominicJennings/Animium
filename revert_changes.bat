4a25719401c9b93ee842741f1a5a1dc8488fbd25
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-09 01:12:20 -0400

    Update revert_changes.bat

--- revert_changes.bat
+++ revert_changes.bat
@@ -1,4 +1,4 @@
 @echo off
 :: this is another set of random batch files that i created. i don't know why. but i am making this just for the devs.
-set /p CHOICE=Please Input Your Recent Commit Code:
+set /p CHOICE=Please Input Your Recent Commit Code. You can get that by going to Animium's Recent Commits:
 git revert !choice!
