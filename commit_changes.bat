d3778dc5759f12d9a8002660b123e50e31bfedfb
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-09 01:19:04 -0400

    Update commit_changes.bat

--- commit_changes.bat
+++ commit_changes.bat
@@ -1,6 +1,5 @@
 @echo off
 :: this is another set of random batch files that i created. i don't know why. but i am making this just for the devs.
 set /p CHOICE=Please Input Your Commit Response:
-git add "%choice%"
 git commit -a -m "%choice%"
 git push
