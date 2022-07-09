840b43b588818e9f73dc3b4b7c009ca3fb312f6e
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-09 01:25:23 -0400

    Update reset.bat

--- devStuff/reset.bat
+++ devStuff/reset.bat
@@ -3,6 +3,7 @@
 :: NOTE: you may want to backup some of your files before using this file.
 @echo off
 echo Reseting Dev Settings....
+cd ..\..\
 git checkout main || echo that's weird. something has failled. 2nd time is the charm.
 git fetch --all || echo this is getting suspicious. now reseting the origin.
 git reset --hard origin/main || an error has occured in all 3 phases. the command prompt is starting now so that way you can type the commands shown below. && call cmd.exe
