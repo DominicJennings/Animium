c320104a9515f7854535ccee0b91528841da60f7
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-09 00:25:50 -0400

    Update reset.bat

--- reset.bat
+++ reset.bat
@@ -1,6 +1,7 @@
 :: Animium Resetter.
 :: a batch file i created for just in case anyone who is contributuing inside of anistick messes something up causing it to break.
 :: NOTE: you may want to backup some of your files before using this file.
+@echo off
 echo Reseting Dev Settings....
 git checkout main || echo that's weird. something has failled. 2nd time is the charm.
 git fetch --all || echo this is getting suspicious. now reseting the origin.
