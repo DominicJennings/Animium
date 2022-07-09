b0b30ae70eb016ec132178b281b2cae924f24d28
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-09 01:11:26 -0400

    Create revert_changes.bat

---
+++ revert_changes.bat
@@ -0,0 +1,4 @@
+@echo off
+:: this is another set of random batch files that i created. i don't know why. but i am making this just for the devs.
+set /p CHOICE=Please Input Your Recent Commit Code:
+git revert !choice!
