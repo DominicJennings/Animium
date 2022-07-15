cde787511f320ee36c621e2f0d6f4e7e91d2b560
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-14 21:32:45 -0400

    Update update.bat

--- update.bat
+++ update.bat
@@ -9,7 +9,7 @@ if exist package-lock.json ( del package-lock.json || echo Error deleting the pa
 :: set error vars if something went wrong.
 if "%ERROR_OCCURED_ONCE%"=="" ( set ERROR_OCCURED_ONCE=y ) else ( set ERROR_OCCURED_TWICE=y )
 :: don't peform the updates after an error occurs.
-if %ERROR_OCCURED_ONCE%==y ( %ERROR_OCCURED_TWICE%==y ( echo There was an error while peforming two tasks. so the update will be skipped. && pause & exit ) else ( echo There was an error while peforming a task. so the update will be skipped. && pause & exit ) )
+if %ERROR_OCCURED_ONCE%==y ( if %ERROR_OCCURED_TWICE%==y ( echo There was an error while peforming two tasks. so the update will be skipped. && pause & exit ) else ( echo There was an error while peforming a task. so the update will be skipped. && pause & exit ) )
 :: peform the update afterwards.
 if exist .git ( git pull || echo Update failed. Attempting to update again... && git stash && git pull || echo i have nothing else to do now. && npm install && call cmd.exe ) else ( echo Git not found. Update skiped. && pause & exit )
 echo Update sucessfull. && npm install && pause & exit
