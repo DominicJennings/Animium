0cc8fdf6c3ef36a0a408af95b358a08a9dc643a5
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-17 13:17:26 -0400

    Okay the first one was a false alarm. now it works

--- asset/load.js
+++ asset/load.js
@@ -1,5 +1,6 @@
 const loadPost = require("../misc/post_body");
 const asset = require("./main");
+const character = require("../character/main");
 const http = require("http");
 
 /**
@@ -11,7 +12,7 @@ const http = require("http");
 module.exports = function (req, res, url) {
 	switch (req.method) {
 		case "GET": {
-			const match = req.url.match(/\/assets\/([^/]+)\/([^.]+)(?:\.xml)?$/);
+			const match = req.url.match(/\/(assets|goapi\/getAsset)\/([^/]+)\/([^.]+)(?:\.xml)?$/);
 			if (!match) return;
 
 			const mId = match[1];
@@ -38,6 +39,23 @@ module.exports = function (req, res, url) {
 						if (b) {
 							res.setHeader("Content-Length", b.length);
 							res.setHeader("Content-Type", "audio/mp3");
+							res.end(b);
+						} else {
+							res.statusCode = 404;
+							res.end();
+						}
+					});
+					return true;
+				}
+				case "/goapi/deleteAsset/": {
+					loadPost(req, res).then(([data, mId]) => {
+						const aId = data.assetId || data.enc_asset_id;
+						/* 
+						honestly, char deleting is for the dev channel anyway and it may break on you anyway as well. 
+						So lets just delete regular assets instead.
+						*/
+						const b = asset.delete(mId, aId);
+						if (b) {
 							res.end(b);
 						} else {
 							res.statusCode = 404;
